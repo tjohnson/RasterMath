ee09f3a62567b7c9ab71a4477cd8687099c8ceb6
Todd Johnson <tjohnson@ball.com>
2009-07-09 14:59:25 -0400

    updated copyright

--- RasterMathGrammar.cpp
+++ RasterMathGrammar.cpp
@@ -1,6 +1,6 @@
 /*
  * The information in this file is
- * Copyright(c) 2007 Ball Aerospace & Technologies Corporation
+ * Copyright(c) 2009 Todd A. Johnson
  * and is subject to the terms and conditions of the
  * GNU Lesser General Public License Version 2.1
  * The license text is available from   
